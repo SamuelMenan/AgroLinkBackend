a9374a30cef1211ac50c63e37b600f20238332cd
SamuelMenan <sam100uel@gmail.com>
2025-11-21 19:55:58 -0500

    Backend changes

--- src/main/java/com/agrolink/config/CorsConfig.java
+++ src/main/java/com/agrolink/config/CorsConfig.java
@@ -22,7 +22,7 @@ public class CorsConfig {
     @Order(Ordered.HIGHEST_PRECEDENCE)
     public CorsFilter globalCorsFilter() {
         Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-        String frontProd = resolveEnv(dotenv, "FRONTEND_ORIGIN").orElse("https://delicate-daifuku-92218d.netlify.app");
+        String frontProd = resolveEnv(dotenv, "FRONTEND_ORIGIN").orElse("https://agro-link-jet.vercel.app");
         String frontDev = resolveEnv(dotenv, "FRONTEND_ORIGIN_DEV").orElse("http://localhost:5173");
 
         CorsConfiguration cfg = new CorsConfiguration();
